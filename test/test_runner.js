b1b1076b846346fd8f87a4ac3630f0d7cbd295da
mirabelrice <rice122@mail.chapman.edu>
2017-03-08 12:45:50 -0800

    added test todo

--- test/test_runner.js
+++ test/test_runner.js
@@ -1,3 +1,7 @@
+// TODO: find a way to test cropping functionality
+// these functions live in js/dimensions.js but requiring this file
+// in a test environment will break tests due to jQuery code dependencies
+
 require(["test/simple_test", "jquery"], function($) {
   window.mochaPhantomJS ? mochaPhantomJS.run() : mocha.run();
 });
